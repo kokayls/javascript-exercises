22531a03bccbd8892a2bd404f6b91cd48f7bcfde
kokayls <kokailaszlodev@gmail.com>
2022-09-05 19:44:11 +0200

    exercise finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+    if (begin > end) {
+        let temp = begin;
+        begin = end;
+        end = temp;
+    }
 
+    if (begin < 0 || end < 0 || typeof begin !== "number" || typeof end !== "number") {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    for (let i = begin; i <= end; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
