2d14a9492975b0a03631ba610ce820b346dbda89
kokayls <kokailaszlodev@gmail.com>
2022-09-05 19:18:34 +0200

    exercise finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,6 +2,4 @@ const reverseString = function(string) {
     return string.split('').reverse().join('');
 };
 
-//console.log(reverseString('123! abc!'));
-// Do not edit below this line
 module.exports = reverseString;
