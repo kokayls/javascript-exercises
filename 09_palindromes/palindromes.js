39c392e5758ff22fd24f15916a44c899395d2d14
kokayls <kokailaszlodev@gmail.com>
2022-09-10 20:08:18 +0200

    finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+    processedString = sentence.toLowerCase().replace(/[^a-z]/g, "");
+  return (
+    processedString
+      .split("")
+      .reverse()
+      .join("") == processedString
+  );
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
