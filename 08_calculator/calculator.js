17054fadeda412ab2950e9941bc2eebf1ded8cac
kokayls <kokailaszlodev@gmail.com>
2022-09-10 19:44:44 +0200

    exercise finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach(element => {
+    sum += element
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let result = 1;
+  numbers.forEach(number => {
+    result *= number;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let f = [];
+  
+	if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = factorial(n-1) * n;
 };
 
 // Do not edit below this line
