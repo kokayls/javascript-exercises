8a0b1e4fa665a459026fa3aa0bac6dc327044ca8
kokayls <kokailaszlodev@gmail.com>
2022-09-11 08:16:14 +0200

    exercise finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+//  let  titles = [];
+//  for (const book of books) {
+//     titles.push(book.title);
+//  }
+//  return titles;
+ return books.map(book => book.title);
 };
 
 // Do not edit below this line
