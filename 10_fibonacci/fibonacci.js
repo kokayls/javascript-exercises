f959d08096f30dada093f3bf77ca69768535750e
kokayls <kokailaszlodev@gmail.com>
2022-09-11 08:05:55 +0200

    exercise finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let num1 = 0;
+    let num2 = 1; 
 
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+
+    
+    for (let i = 1; i < n; i++) {
+        let temp = num2;
+        num2 = num1 + num2;
+        num1 = temp;
+    }
+    return num2;
 };
 
 // Do not edit below this line
